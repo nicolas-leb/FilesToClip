d92df9d6feff1f2787d02f6c2778ae81a3d56fc6
nicolas-leb <nicolas.lebrun@posteo.net>
2025-02-11 11:16:38 +0100

    call FileConcatenator from the command handler

--- src/FilesToClip/Program.cs
+++ src/FilesToClip/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.CommandLine;
+﻿using FilesToClip;
+using System.CommandLine;
+using System.IO.Abstractions;
 
 var extensionsOption = new Option<string>(
         "--extension",
@@ -10,16 +12,22 @@ extensionsOption.AddAlias("-e");
 
 var scanFolderOption = new Option<string>(
         "--folder",
-        "The folder to scan for files.");
+        "The folder to scan for files.")
+{
+    IsRequired = true,
+};
 scanFolderOption.AddAlias("-sf");
 
 var rootCommand = new RootCommand("CLI tool to concatenate files by extension into the system clipboard.");
 rootCommand.AddOption(extensionsOption);
 rootCommand.AddOption(scanFolderOption);
 
-rootCommand.SetHandler(() =>
+rootCommand.SetHandler(async (extensionsOptionValue, scanFolderValue) =>
 {
-    Console.WriteLine("Hello, World!");
-});
+    Console.WriteLine($"Scanning {scanFolderValue} for {extensionsOptionValue} files!");
+    var fileContatenator = new FileContatenator(new FileSystem());
+    await fileContatenator.ConcatenateFilesAsync(extensionsOptionValue, scanFolderValue);
+},
+    extensionsOption, scanFolderOption);
 
 return await rootCommand.InvokeAsync(args);
