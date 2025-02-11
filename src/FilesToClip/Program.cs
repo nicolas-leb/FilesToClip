b4c83ba91eb718b69f063fb457106f59a8b9b889
nicolas-leb <nicolas.lebrun@posteo.net>
2025-02-11 11:01:52 +0100

    add root command with options using System.CommandLine

--- src/FilesToClip/Program.cs
+++ src/FilesToClip/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+var extensionsOption = new Option<string>(
+        "--extension",
+        "The file extension to search for (e.g., .txt)")
+{
+    IsRequired = true,
+};
+extensionsOption.AddAlias("-e");
+
+var scanFolderOption = new Option<string>(
+        "--folder",
+        "The folder to scan for files.");
+scanFolderOption.AddAlias("-sf");
+
+var rootCommand = new RootCommand("CLI tool to concatenate files by extension into the system clipboard.");
+rootCommand.AddOption(extensionsOption);
+rootCommand.AddOption(scanFolderOption);
+
+rootCommand.SetHandler(() =>
+{
+    Console.WriteLine("Hello, World!");
+});
+
+return await rootCommand.InvokeAsync(args);
