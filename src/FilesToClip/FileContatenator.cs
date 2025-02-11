895bf7270cdb10437b1b06a3159697d5aa87037d
nicolas-leb <nicolas.lebrun@posteo.net>
2025-02-11 11:16:19 +0100

    implement FileConcatenator

---
+++ src/FilesToClip/FileContatenator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextCopy;
+
+namespace FilesToClip
+{
+    internal class FileContatenator
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public FileContatenator(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+
+        public async Task ConcatenateFilesAsync(string extension, string folderPath)
+        {
+
+            if (!_fileSystem.Directory.Exists(folderPath))
+            {
+                return;
+            }
+
+            var files = _fileSystem.Directory.GetFiles(folderPath, $"*{extension}");
+            if (files.Length == 0)
+            {
+                return;
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            foreach (var file in files)
+            {
+                using var reader = _fileSystem.File.OpenText(file);
+                string content = await reader.ReadToEndAsync();
+
+                stringBuilder.AppendLine(content);
+            }
+
+            string mergedContent = stringBuilder.ToString();
+
+            // Copy the merged content to the clipboard
+            ClipboardService.SetText(mergedContent);
+
+        }
+    }
+}
